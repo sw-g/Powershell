abe78a3675479a0f3ffc0e063bbc91b8a4cae8a5
mvanson <m.vanson@nerdherd.nl>
2014-07-08 12:13:24 +0200

    Initial commit

---
+++ Active Directory/reportComputersLastLogon.ps1
@@ -0,0 +1,6 @@
+####################################################################
+# Author: Michel van Son                                           #
+# Date: 08-07-2014                                                 #
+# Function: Reports last logon time for computer objects           #
+# Changes: [DD-MM-YYYY] Name - Changes                             #
+####################################################################
---
+++ Active Directory/reportUsersLastLogon.ps1
@@ -0,0 +1,3 @@
+. "\\ajsfile01\software$\powershell scripts\functions\sendMail.ps1"
+
+sendMail -to "michel.vanson@ajs-isc.com" -subject "test email" -body "gewoon een test"
